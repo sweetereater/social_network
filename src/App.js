a5545ccbc1afbad8706f5ecee40c3f8bf6376524
sweetereater <sweeter_2093@mail.ru>
2021-07-07 23:42:37 +0300

    Simple layout (Lesson 11)

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import Technologies from './Components/Technologies';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Technologies />
+      <header className='header'>
+        <img src="../public/logo_neu_de.png" alt="" />
+      </header>
+      <nav className="side">
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+      </nav>
+      <main className="main">
+        <Header />
+        <Technologies />
+      </main>
+
     </div>
   );
 }
