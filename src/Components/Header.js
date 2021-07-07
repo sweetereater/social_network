02fd1de1cc4227595da76e4271fae9152a83f1f0
sweetereater <sweeter_2093@mail.ru>
2021-07-07 22:57:21 +0300

    deleted

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <div>
+            <a href="#">Home - </a>
+            <a href="#">News - </a>
+            <a href="#">Messages - </a>
+            <a href="#">Contacts </a>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Components/Technologies.js
@@ -0,0 +1,12 @@
+function Technologies() {
+  return (
+    <ul>
+      <li>html</li>
+      <li>css</li>
+      <li>Javascript</li>
+      <li>React.js</li>
+    </ul>
+  )
+}
+
+export default Technologies;
