9333562a932e27a794a9b328c62e8982cc843461
sweetereater <sweeter_2093@mail.ru>
2021-07-07 22:22:42 +0300

    added 2 components (Header,Technologies) in App.js (Lesson 7)

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with it-incubator!
-        </a>
-      </header>
+      <Header />
+      <Technologies />
     </div>
   );
+}
+
+function Header() {
+  return (
+    <div>
+      <a href="#">Home - </a>
+      <a href="#">News - </a>
+      <a href="#">Contacts </a>
+    </div>
+  )
+}
+
+function Technologies() {
+  return (
+    <ul>
+      <li>html</li>
+      <li>css</li>
+      <li>Javascript</li>
+      <li>React.js</li>
+    </ul>
+  )
 }
 
 export default App;
