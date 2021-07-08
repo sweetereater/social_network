8e3ee6de231183face951663e9b13b52f6b72d3f
sweetereater <sweeter_2093@mail.ru>
2021-07-08 12:33:00 +0300

    Модульность (Lesson 15)

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,21 +1,23 @@
 import s from './Profile.module.css';
+import MyPosts from '../MyPosts/MyPosts';
 
 const Profile = () => {
     return (
         <main className={s.main}>
-            <img src="https://klike.net/uploads/posts/2019-06/1559370578_1.jpg" alt="" />
-            <div>
-                ava + descriptions
-            </div>
-            <div className='posts'>
-                My Posts
-                <div className={s.post}>
-                    Hello World!
+            <img className={s.main__img} src="https://klike.net/uploads/posts/2019-06/1559370578_1.jpg" alt="" />
+            <div className={s.profile_info}>
+                <div className={s.img_container}>
+                    <img
+                        src="https://legacy.mortalkombatonline.com/content/games/mk3/cyrax/bio.gif"
+                        alt=""
+                        className={s.img_container__img}
+                    />
                 </div>
-                <div className={s.post}>
-                    How are you?
+                <div className={s.desc}>
+                    <h1>Privet!</h1>
                 </div>
             </div>
+            <MyPosts />
         </main>
     )
 }
